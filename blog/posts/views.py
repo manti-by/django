d92b65b70dcfed5fff1f1454ac25632bfb919561
Alexander Chaika <manti.by@gmail.com>
2022-01-12 21:57:13 +0300

    Add Django app skeleton

---
+++ blog/posts/views.py
@@ -0,0 +1,11 @@
+import logging
+from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
+
+
+def posts_index(request):
+    value = request.GET.get("key")
+    logger.info(value)
+    return HttpResponse("Posts index view")
+
