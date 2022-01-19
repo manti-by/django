52f0cdaf0f86abb870f01e7050892e720e2cb822
Alexander Chaika <manti.by@gmail.com>
2022-01-19 20:12:22 +0300

    Add homework

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, Tags
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    fields = ("title",)
+    search_fields = ("title",)
+
+
+class TagsAdminInline(admin.TabularInline):
+    model = Tags.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "slug", "created_at")
+    fields = ("author", "title", "image", "slug", "text", "created_at")
+    readonly_fields = ("created_at",)
+    search_fields = ("title", "slug", "text")
+
+    inlines = (TagsAdminInline,)
