b2f94bcd3058d9c0c4a00ba29dde9d20e01318d8
Alexander Chaika <manti.by@gmail.com>
2022-02-02 16:16:58 +0300

    Add profiles API

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import PostViewSet
+from api.posts.views import PostViewSet
+from api.profiles.views import ProfileViewSet
+from api.users.views import UserViewSet
 
 app_name = "api"
 
 router = routers.DefaultRouter()
-router.register(r"posts", PostViewSet)
+router.register(r"posts", PostViewSet, basename="posts")
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"profiles", ProfileViewSet, basename="profiles")
 
 
 urlpatterns = [
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'blog',
     'posts',
+    'profiles',
 ]
 
 MIDDLEWARE = [
