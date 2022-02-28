9312c99d9c6a42c88bb7d21045f285656c8b5683
Alexander Chaika <manti.by@gmail.com>
2022-02-28 21:34:20 +0300

    Add products api filters

--- blog/api/shop/serializers.py
+++ blog/api/shop/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from shop.models import Product
+from shop.models import Product, ORDER_BY_CHOICES
 
 
 class PurchaseCreateSerializer(serializers.Serializer):
@@ -11,3 +12,17 @@ class ProductModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "title", "cost", "external_id", "description", "image", "link", "status"]
+
+
+class ProductFiltersSerializer(serializers.Serializer):
+    cost__gt = serializers.IntegerField(min_value=0, label="Cost Min", required=False)
+    cost__lt = serializers.IntegerField(min_value=0, label="Cost Max", required=False)
+    order_by = serializers.ChoiceField(choices=ORDER_BY_CHOICES, required=False)
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        cost__gt = attrs.get("cost__gt")
+        cost__lt = attrs.get("cost__lt")
+        if cost__gt and cost__lt and cost__gt > cost__lt:
+            raise ValidationError("Min price can't be greater than Max price")
+        return attrs
